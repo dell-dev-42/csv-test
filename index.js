2c65657fb6260d79306265bb93104bb981c09f2c
Klincher <camelotvm48@gmail.com>
2022-10-20 11:56:51 +0300

    finish

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const fs = require("fs");
 const csv = require("csvtojson");
 const Excel = require("exceljs");
 const csvWorksnapsFilePath = "./worksnaps.csv";
 const csvUpworkFilePath = "./upwork.csv";
 const workbook = new Excel.Workbook();
 const worksheet = workbook.addWorksheet("ExampleSheet");
-const lodash = require('lodash');
+const lodash = require("lodash");
 
 function toMS(str) {
   if (!str.includes(":")) return parseFloat(str);
@@ -13,45 +14,66 @@ function toMS(str) {
   return (+mins * 60 + +sec) / 60;
 }
 
-(async () => {
-  const jsonWorksnaps = await csv().fromFile(csvWorksnapsFilePath);
-  const jsonUpwork = await csv().fromFile(csvUpworkFilePath);
+if (fs.existsSync(csvWorksnapsFilePath)) {
+  (async () => {
+    const jsonWorksnaps = await csv().fromFile(csvWorksnapsFilePath);
 
-  const worksnapsData = jsonWorksnaps
-    .filter(
-      (key) => key.field7 != null && key.field7 !== " " && key.field4 != "Date"
-    )
-    .map((el) => ({
-      date_worksnaps: el.field4,
-      time_worksnaps: toMS(el.field7),
-    }));
+    const worksnapsData = jsonWorksnaps
+      .filter(
+        (key) =>
+          key.field7 != null && key.field7 !== " " && key.field4 != "Date"
+      )
+      .map((el) => ({
+        date_worksnaps: el.field4,
+        time_worksnaps: toMS(el.field7),
+      }));
 
-  const upworkData = jsonUpwork.map((el) => ({
-    date_upwork: el.Date,
-    time_upwork: parseFloat(el.Hours),
-  }));
+    let data = worksnapsData;
 
-  // add column headers
-  worksheet.columns = [
-    { header: "Date Worksnaps", key: "date_worksnaps" },
-    { header: "Time Worksnaps", key: "time_worksnaps" },
-    { header: "Time Upwork", key: "date_upwork" },
-    { header: "Time Upwork", key: "time_upwork" },
-  ];
+    // add column headers
+    worksheet.columns = [
+      { header: "Date Worksnaps", key: "date_worksnaps" },
+      { header: "Time Worksnaps", key: "time_worksnaps" },
+      { header: "Time Upwork", key: "date_upwork" },
+      { header: "Time Upwork", key: "time_upwork" },
+    ];
 
-  // Add row using key mapping to columns
-  worksheet.addRows(lodash.merge(worksnapsData, upworkData));
+    if (fs.existsSync(csvUpworkFilePath)) {
+      const jsonUpwork = await csv().fromFile(csvUpworkFilePath);
+      const upworkData = jsonUpwork.map((el) => ({
+        date_upwork: el.Date,
+        time_upwork: parseFloat(el.Hours),
+      }));
 
-  worksheet.getCell("B25").value = { formula: "SUM(B2:B24)", result: 7 };
-  worksheet.getCell("D25").value = { formula: "SUM(D2:D24)", result: 7 };
+      data = lodash.merge(worksnapsData, upworkData);
+    }
 
-  // save workbook to disk
-  workbook.xlsx
-    .writeFile("report.xlsx")
-    .then(() => {
-      console.log("File created.");
-    })
-    .catch((err) => {
-      console.log("err", err);
-    });
-})();
+    // Add row using key mapping to columns
+    worksheet.addRows(data);
+
+    worksheet.getCell("B27").value = { formula: "SUM(B2:B26)" };
+    worksheet.getCell("D27").value = { formula: "SUM(D2:D26)" };
+
+    worksheet.getCell("A28").value = "Rate:";
+    worksheet.getCell("A29").value = "Total:";
+    worksheet.getCell("B28").value = 3;
+    worksheet.getCell("D28").value = 3.5;
+
+    worksheet.getCell("B29").value = { formula: "(B27*B28)" };
+    worksheet.getCell("D29").value = { formula: "(D27*D28)" };
+
+    worksheet.getCell("F29").value = { formula: "SUM(B29,D29)" };
+
+    // save workbook to disk
+    workbook.xlsx
+      .writeFile("report.xlsx")
+      .then(() => {
+        console.log("File created.");
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  })();
+} else {
+  console.log("Зауснь файл в папку");
+}
